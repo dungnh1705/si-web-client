7386f1bb069b51064c0b848baa17aa04a7111191
Jos Nguyễn <dung.nh1705@gmail.com>
2023-08-01 22:10:11 +0700

    fix: change UI for Dashboard Phân Đoàn Trưởng

--- src/pages/Dashboard/PhanDoanTruong/GroupInfo.js
+++ src/pages/Dashboard/PhanDoanTruong/GroupInfo.js
@@ -13,8 +13,8 @@ import CountUp from 'react-countup'
 
 export default function GroupInfo({ info }) {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} lg={6}>
+    <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}>
+      <Grid item xs={6} lg={4}>
         <Card className="card-box border-0 card-shadow-first p-4 mb-4">
           <div className="d-flex align-items-center">
             <div className="d-40 rounded-circle bg-first text-white text-center font-size-lg mr-3">
@@ -30,7 +30,7 @@ export default function GroupInfo({ info }) {
           </div>
         </Card>
       </Grid>
-      <Grid item xs={12} lg={6}>
+      <Grid item xs={6} lg={4}>
         <Card className="card-box border-0 card-shadow-first p-4 mb-4">
           <div className="d-flex align-items-center">
             <div className="d-40 rounded-circle bg-warning text-white text-center font-size-lg mr-3">
@@ -46,7 +46,7 @@ export default function GroupInfo({ info }) {
           </div>
         </Card>
       </Grid>
-      <Grid item xs={12} lg={6}>
+      <Grid item xs={6} lg={4}>
         <Card className="card-box border-0 card-shadow-first p-4 mb-4">
           <div className="d-flex align-items-center">
             <div className="d-40 rounded-circle bg-primary text-white text-center font-size-lg mr-3">
@@ -62,7 +62,23 @@ export default function GroupInfo({ info }) {
           </div>
         </Card>
       </Grid>
-      <Grid item xs={12} lg={6}>
+      <Grid item xs={6} lg={4}>
+        <Card className="card-box border-0 card-shadow-first p-4 mb-4">
+          <div className="d-flex align-items-center">
+            <div className="d-40 rounded-circle bg-primary text-white text-center font-size-lg mr-3">
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+            <div className="text-black-50">Đoàn sinh ở lại</div>
+          </div>
+          <div className="display-3 text-center line-height-sm text-second text-center d-flex align-items-center pt-3 justify-content-center">
+            <CountUp start={0} end={_.sumBy(info?.summaryInfo, i => i.totalStayInClass)} duration={2} separator="" decimals={0} decimal="" prefix="" suffix="" />
+          </div>
+          <div className="text-black-50 text-center opacity-6 pt-3">
+            <b>{accounting.toFixed((_.sumBy(info?.summaryInfo, i => i.totalStayInClass) / _.sumBy(info?.summaryInfo, i => i.totalStudent)) * 100, 1)} %</b>
+          </div>
+        </Card>
+      </Grid>
+      <Grid item xs={6} lg={4}>
         <Card className="card-box border-0 card-shadow-first p-4 mb-4">
           <div className="d-flex align-items-center">
             <div className="d-40 rounded-circle bg-danger text-white text-center font-size-lg mr-3">
